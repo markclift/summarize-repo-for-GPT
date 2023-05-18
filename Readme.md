285b60912651368e3b960401444814d938628d94
“markclift” <mark.clift.uk@gmail.com>
2023-05-18 22:20:31 +0100

    Added ability to filter by file extension

--- Readme.md
+++ Readme.md
@@ -1,6 +1,6 @@
 # GitHub Repo File Downloader
 
-This Python script allows you to download the contents of all files in a GitHub repository.
+This Python script allows you to download the contents of specific types of files in a GitHub repository.
 
 ## Prerequisites
 
@@ -15,7 +15,7 @@ This Python script allows you to download the contents of all files in a GitHub 
 1. Clone the repository or download the script directly.
 2. Install the required dependencies by running the following command:
 ```python
-pip install requirements.txt
+pip install -r requirements.txt
 ```
 
 ## Usage
@@ -29,11 +29,15 @@ python3 summarize_repo.py
 
 3. After entering the URL, the script will authenticate using the GitHub token stored in the `.env` file.
 
-4. It will recursively fetch the contents of all files in the repository, excluding the ignored files specified in the `IGNORED_FILES` list.
+4. It will recursively go through the repository to gather all distinct file extensions (excluding those in the IGNORED_FILES list).
 
-5. The script will save the contents of the files to a text file. You will be prompted to select the destination path and provide a filename.
+5. A GUI will be opened listing all file extensions in the repository. All extensions will be ticked by default. You can select which file types you want to include in the download by ticking or unticking the boxes next to the extensions.
 
-6. The text file will contain the file paths and their respective contents, separated by the "===============================" delimiter.
+6. Once you confirm your selection, the script will fetch the contents of only the files of the selected types.
+
+7. You will be prompted to select the destination path and provide a filename. The contents of the files will then be saved to a text file at the chosen location.
+
+8. The text file will contain the file paths and their respective contents, separated by the "===============================" delimiter.
 
 **Note:** Ensure that you have the necessary access permissions to the repository.
 
--- summarize_repo.py
+++ summarize_repo.py
@@ -14,46 +14,60 @@ load_dotenv()
 
 USER_TOKEN = os.getenv('GITHUB_TOKEN')
 
-IGNORED_FILES=['.gitignore', 'package-lock.json', 'tsconfig.json', 'vercel.svg', 'favicon.ico']
+IGNORED_FILES = ['.gitignore', 'package-lock.json', 'tsconfig.json', 'vercel.svg', 'favicon.ico']
 
-def get_all_files_recursive(repo, path=''):
+def get_all_files_recursive(repo, path='', selected_extensions=None):
     output = ''
+    extensions = set()
     contents = repo.get_contents(path)
 
     for content in contents:
         if content.type == 'dir':
-            output += get_all_files_recursive(repo, content.path)
-        else:
-            if content.name not in IGNORED_FILES:
-                output += "File path: " + content.path + '\nFile content:\n\n'
-                output += get_content_of_file(content.download_url)
-                output += "\n\n===============================\n\n"
+            new_output, new_extensions = get_all_files_recursive(repo, content.path, selected_extensions)
+            output += new_output
+            extensions.update(new_extensions)
+        elif content.name not in IGNORED_FILES:
+            extension = os.path.splitext(content.name)[-1]
+            extensions.add(extension)
+            if selected_extensions is not None and extension not in selected_extensions:
+                continue
+            output += "File path: " + content.path + '\nFile content:\n\n'
+            output += get_content_of_file(content.download_url)
+            output += "\n\n================\n\n"
 
-    return output
+    return output, extensions
 
 def get_content_of_file(file_url):
     response = requests.get(file_url)
 
-    # Try to decode the content to text
     try:
         response.content.decode('utf-8')
     except UnicodeDecodeError:
-        # If decoding fails, it's likely a binary file, so return an empty string
         return ''
     else:
-        # If decoding succeeds, it's likely a text file, so return the content
         return response.text
 
+def extension_selection_dialog(extensions):
+    window = tk.Toplevel()
+    window.title("Select file extensions")
+    check_vars = {ext: tk.BooleanVar(window, True) for ext in extensions}  # default to True
+    for ext, var in check_vars.items():
+        tk.Checkbutton(window, text=ext, variable=var).pack()
+    tk.Button(window, text="OK", command=window.destroy).pack()
+    window.wait_window()  # wait for window to be destroyed
+    return {ext for ext, var in check_vars.items() if var.get()}  # get selected extensions
 
 def main():
-    repo_url = simpledialog.askstring(title="Github Repo",
-                                      prompt="Please enter the Github Repo URL:")
+    repo_url = simpledialog.askstring(title="Github Repo", prompt="Please enter the Github Repo URL:")
 
     repo_name = repo_url.split('https://github.com/')[-1]
     g = Github(USER_TOKEN)
     repo = g.get_repo(repo_name)
 
-    output = get_all_files_recursive(repo)
+    _, all_extensions = get_all_files_recursive(repo)
+    selected_extensions = extension_selection_dialog(all_extensions)
+
+    output, _ = get_all_files_recursive(repo, selected_extensions=selected_extensions)
 
     file_path = filedialog.asksaveasfilename(defaultextension='.txt', 
                                              filetypes=[('Text Files', '*.txt'), ('All Files', '*.*')])
